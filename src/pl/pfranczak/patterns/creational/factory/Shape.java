5a2dad76c6f47cd58dee78f632a575244d1ab8ec
Pawel Franczak <gzem89@gmail.com>
2021-12-27 10:08:13 +0100

    Factory Pattern

---
+++ src/pl/pfranczak/patterns/creational/factory/Shape.java
@@ -0,0 +1,5 @@
+package pl.pfranczak.patterns.creational.factory;
+
+public interface Shape {
+	void draw();
+}
---
+++ src/pl/pfranczak/patterns/creational/factory/ShapeFactory.java
@@ -0,0 +1,25 @@
+package pl.pfranczak.patterns.creational.factory;
+
+public class ShapeFactory {
+
+	public Shape getShape(String shapeType) {
+		if(shapeType == null){
+			return null;
+		}
+		
+		if(shapeType.equalsIgnoreCase("CIRCLE")){
+		   return new Circle();
+		}
+		
+		if(shapeType.equalsIgnoreCase("RECTANGLE")){
+		   return new Rectangle();
+		}
+		
+		if(shapeType.equalsIgnoreCase("SQUARE")){
+		   return new Square();
+		}
+		
+		return null;
+	}
+	
+}
